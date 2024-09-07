00b7e412e933d069b0ca9391bf9b20c218ac4f0b
SuYxh <46957352+SuYxh@users.noreply.github.com>
2024-09-07 15:52:08 +0800

    Deploying to gh-pages from  @ 869901a7413563c231cad87a1b93c7834653f958 🚀

---
+++ assets/develop_index.md.DICIy90Y.js
@@ -0,0 +1,13 @@
+import{_ as a,c as n,a1 as e,o as p}from"./chunks/framework.B9Kr-3uN.js";const h=JSON.parse('{"title":"目录结构","description":"","frontmatter":{},"headers":[],"relativePath":"develop/index.md","filePath":"develop/index.md"}'),t={name:"develop/index.md"};function l(i,s,c,o,d,r){return p(),n("div",null,s[0]||(s[0]=[e(`<h1 id="目录结构" tabindex="-1">目录结构 <a class="header-anchor" href="#目录结构" aria-label="Permalink to &quot;目录结构&quot;">​</a></h1><br><br><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>-- packages</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- cli 脚手架(create-easyest)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- components 本地开发组件库(@easyest/components)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- easyest 打包后组件库,既要发布的组件库(easyest)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- utils 工具包(@easyest/utils)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>-- play 测试开发组件库的Vue3项目目录</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>-- site 组件库文档站点目录</span></span></code></pre></div>`,4)]))}const m=a(t,[["render",l]]);export{h as __pageData,m as default};
